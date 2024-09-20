c20b100367fad7f9196de9ff4dffe3747c4867bd
Saurabh Maurya <mauryasaurabh9698@gmail.com>
2024-09-20 10:48:08 +0530

    Update Q2-Sum_naturalno.c
    Adding comments so that the code is easy to understand

--- Q2-Sum_naturalno.c
+++ Q2-Sum_naturalno.c
@@ -1,10 +1,11 @@
 //Program to Calculate the Sum of Natural Numbers .
 
-#include<stdio.h>
+#include<stdio.h> 
+//below here we create a function name "sum" for finding sum of natural number.
 int sum(int n)
 {
     int j=0;
-
+    //Create for loop for sum it till the max user number adds.
     for (int i = 1;( i = n); i++)
     {
         j += n;
@@ -14,10 +15,14 @@ int sum(int n)
 }
 int main()
 {
+    ///In below line We create a Intger variabe named "num" .
     int num;
+    //In below line we display this msg on user screen and ask user to input max number for sum .
     printf("Enter the max number for sum : ");
-    scanf("%d", &num);
+    scanf("%d", &num); // in this line we take input from user and store it in "num" variable.
+    //In below line we display the sum of natural number on user screen. also here we run "sum" function for finding the sum.
     printf("The sum of natural number from 1 to %d is %d",num, sum(num));
     return 0;
 
 }
+//PROGRAM ENDS HERE
