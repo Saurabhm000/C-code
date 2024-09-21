45d52beaa6e82c1020f4fc06acb00a477559db85
Saurabh Maurya <mauryasaurabh9698@gmail.com>
2024-09-21 11:04:13 +0530

    Update Q3-size_of_intcharfloat.c
    Added comments thats makes the code easy to understand

--- Q3-size_of_intcharfloat.c
+++ Q3-size_of_intcharfloat.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 int main()
 {
+    //Below , we create diffferent different different data type variable
     int a;
     char b;
     float c;
     long d;
     double e;
     unsigned f;
-    printf("Size of int is : %lu\n", sizeof a);
-    printf("Size of long is : %lu\n", sizeof d);
-    printf("Size of char is : %lu\n", sizeof b);
-    printf("Size of float : %lu\n", sizeof c);
-    printf("Size of double : %lu\n", sizeof e);
-    printf("Size of unsigned  : %lu\n", sizeof f);
+    //After Declaring the variables type and name 
+    // we display it on the screen by using printf fucntion.
+    printf("Size of int is : %lu\n", sizeof a); //here we use 'size of' operator thats display the size of a.
+    printf("Size of long is : %lu\n", sizeof d); //here we use 'size of' operator thats display the size of d.
+    printf("Size of char is : %lu\n", sizeof b); //here we use 'size of' operator thats display the size of b.
+    printf("Size of float : %lu\n", sizeof c); //here we use 'size of' operator thats display the size of c.
+    printf("Size of double : %lu\n", sizeof e); //here we use 'size of' operator thats display the size of e.
+    printf("Size of unsigned  : %lu\n", sizeof f); //here we use 'size of' operator thats display the size of f.
 
     return 0;
 }
