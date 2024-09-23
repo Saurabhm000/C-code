6ca34c73d2949a72680513d406ea3ea74c0e9178
Saurabh Maurya <mauryasaurabh9698@gmail.com>
2024-09-23 09:52:12 +0530

    Update Q4-Swapping_value.c
    Comments added for easy understanding of code

--- Q4-Swapping_value.c
+++ Q4-Swapping_value.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main() {
-    int a, b, c ;
+    int a, b, c ; //  declaring variabel type and name .
     printf("Enter the value of a : ");
-    scanf("%d", &a);
+    scanf("%d", &a); //take the input value from user .
     printf("Enter the value of b : ");
-    scanf("%d", &b);
-    printf("the value of a is %d and b is %d \n", a, b);
+    scanf("%d", &b); //take the input value from user .
+    printf("the value of a is %d and b is %d \n", a, b); // Display the value before swapping .
     printf("after swapping the value of a and b\n");
-    c=a;
-    a=b;
-    b=c;
-    printf("the value of a is %d and b is %d", a, b);
+    c=a; //putting the values of a into c.
+    a=b; //putting the value of b into a .
+    b=c; //putting the value of c into b.
+    printf("the value of a is %d and b is %d", a, b); // Display the value after swapping .
     return 0;
 }
