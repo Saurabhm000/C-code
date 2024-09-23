be47d87b53367996fafe0886b3199e11ee0ec7aa
Saurabh Maurya <mauryasaurabh9698@gmail.com>
2024-09-23 10:13:55 +0530

    Update Q5-Voting_Eligibility.c
    Added comments for easy understanding of code

--- Q5-Voting_Eligibility.c
+++ Q5-Voting_Eligibility.c
@@ -2,20 +2,20 @@
 int main()
 {
     int age;
-    printf("Enter your age ");
-    scanf("%d", &age);
+    printf("Enter your age "); //ask the user to enter the age
+    scanf("%d", &age); //save the value in 'age' variable
     printf("you have entered %d as your age\n", age);
-    if (age>=18)
+    if (age>=18) //using if statement to check age is 18 or greater than 18.
     {
         printf("You are eligible for vote\n");
     }
-    else if (age>=10)
+    else if (age>=10) //using else if statement to check age is 10 or greater than 10.
     {
         printf("You're a  Teenager not an adult");
     }
     
 
-    else
+    else // Using 'else' statement, if above condition is not true then it enters in else condition.
     {
         printf("No you are not eligibe for vote");
     }
